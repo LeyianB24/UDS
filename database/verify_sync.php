2a46a41f56709974bd1065e2b55551b01e5a876a
Bezalel Leyian <158043641+LeyianB24@users.noreply.github.com>
2026-04-03 16:16:25 +0300

    refactor: remove unused files and associated references

--- database/verify_sync.php
+++
@@ -1,7 +0,0 @@
-<?php
-$c = new mysqli('localhost', 'root', '', 'umoja-drivers-sacco');
-$res = $c->query("SHOW TABLES");
-echo "Count: " . $res->num_rows . "\n";
-while($row = $res->fetch_row()) {
-    echo $row[0] . "\n";
-}
